fc492a01c86d9e7287e724f8da35740748e0af20
Bruno Belchior <brunovicenteb@gmail.com>
2022-07-19 15:01:54 -0300

    Projetos de testes adicionados.

---
+++ src/Benefit.Test/Toolkit/Data/BaseEntityTest.cs
@@ -0,0 +1,38 @@
+﻿using Toolkit.Data;
+
+namespace Benefit.Test.Toolkit.Data;
+public class BaseEntityTest
+{
+    #region NewBaseEntityTest
+    private class NewBaseEntityTest : BaseEntity
+    {
+        public NewBaseEntityTest() :
+            base()
+        {
+        }
+        public NewBaseEntityTest(string id) :
+            base(id)
+        {
+        }
+    }
+    #endregion
+
+    [Theory]
+    [InlineData("0")]
+    [InlineData("25")]
+    [InlineData("43")]
+    public void CreatorTest(string id)
+    {
+        var newBaseEntity = new NewBaseEntityTest(id);
+        Assert.Equal(id, newBaseEntity.ID);
+    }
+
+    [Fact]
+    public void GetValidatorsTest()
+    {
+        var newBaseEntity = new NewBaseEntityTest();
+        var validators = newBaseEntity.GetValidators();
+        Assert.NotNull(validators);
+        Assert.Empty(validators);
+    }
+}
---
+++ src/Benefit.Test/Toolkit/StringTest.cs
@@ -0,0 +1,110 @@
+﻿using System.Linq;
+using Toolkit;
+
+namespace Benefit.Test.Toolkit
+{
+    public class StringTest
+    {
+        [Theory]
+        [InlineData("Has value!", false)]
+        [InlineData("", true)]
+        [InlineData(" ", false)]
+        [InlineData("Also has value!", false)]
+        [InlineData(null, true)]
+        public void TestIsEmptyValueFunction(string valueForTest, bool expectedValue)
+        {
+            //arrange
+
+            //act
+            var result = valueForTest.IsEmpty();
+
+            //assert
+            Assert.IsType<bool>(result);
+            Assert.Equal(expectedValue, result);
+
+            if (result)
+                Assert.True(String.IsNullOrEmpty(valueForTest));
+            else
+                Assert.NotEmpty(valueForTest);
+        }
+
+        [Theory]
+        [InlineData("Has value!", true)]
+        [InlineData("", false)]
+        [InlineData(" ", true)]
+        [InlineData("Also has value!", true)]
+        [InlineData(null, false)]
+        public void TestIsFilledValueFunction(string valueForTest, bool expectedValue)
+        {
+            //arrange
+
+            //act
+            var result = valueForTest.IsFilled();
+
+            //assert
+            Assert.IsType<bool>(result);
+            Assert.Equal(expectedValue, result);
+
+            if (result)
+                Assert.NotEmpty(valueForTest);
+            else
+                Assert.True(String.IsNullOrEmpty(valueForTest));
+        }
+
+        [Theory]
+        [InlineData("is lower!", "is lower!")]
+        [InlineData("IS UPPER", "is upper")]
+        [InlineData("iT's bOth", "it's both")]
+        [InlineData("", "")]
+        [InlineData(null, "")]
+        public void TestSafeToLowerFunction(string valueForTest, string expectedValue)
+        {
+            //arrange
+
+            //act
+            var result = valueForTest.SafeToLower();
+            var resultOnlyLetters = new String(result.Where(c => Char.IsLetter(c)).ToArray());
+
+            //assert
+            Assert.IsType<string>(result);
+            Assert.True(resultOnlyLetters.All(char.IsLower));
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData("481.431.670-46")]
+        [InlineData("890.564.540-21")]
+        [InlineData("496.851.180-94")]
+        [InlineData("692.144.560-70")]
+        public void TestValidCpf(string valueForTest)
+        {
+            //arrange
+
+            //act
+            var validDocument = valueForTest.IsValidCPF();
+
+            //assert
+            Assert.IsType<bool>(validDocument);
+            Assert.True(validDocument);
+        }
+
+        [Theory]
+        [InlineData("481.431.670")]
+        [InlineData("190.564.540-21")]
+        [InlineData("557.851.180-94")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void TestInvalidCpf(string valueForTest)
+        {
+            //arrange
+
+            //act
+            var validDocument = valueForTest.IsValidCPF();
+
+            //assert
+            Assert.IsType<bool>(validDocument);
+            Assert.False(validDocument);
+        }
+
+    }
+}
